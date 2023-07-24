6e04950c1101a4dd99f96f2494b3dd1c2b7d8304
Saklain Ahmad <saklainhmd@gmail.com>
2023-07-24 13:16:56 +0530

    login form done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,73 @@
+import { useState } from 'react';
 import styles from '../styles/login.module.css';
+import { useToasts } from 'react-toast-notifications';
 
+import { login } from '../api';
 
 const Login = () => {
-    return <form className={styles.loginForm}>
-        <span className={styles.loginSignupHeader}>Log In</span>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+  const { addToast } = useToasts();
 
-        <div className={styles.field}>
-            <input  type='email' placeholder='Email' required/>
-        </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        <div className={styles.field}>
-            <input  type='password' placeholder='Password' required/>
-        </div>
-        
-        <div className={styles.field}>
-            <button>Log In</button>
-        </div>
+    setLoggingIn(true);
+
+    if (!email || !password) {
+      return addToast('Please enter both  email and password', {
+        appearance: 'error',
+      });
+    }
+
+    const response = await login(email, password);
+
+    if (response.success) {
+      addToast('Successfully logged in ', {
+        appearance: 'success',
+      });
+    } else {
+      addToast(response.message, {
+        appearance: 'error',
+      });
+    }
+
+    setLoggingIn(false);
+  };
+
+  return (
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
+      <span className={styles.loginSignupHeader}>Log In</span>
+
+      <div className={styles.field}>
+        <input
+          type="email"
+          placeholder="Email"
+          //   required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className={styles.field}>
+        <input
+          type="password"
+          placeholder="Password"
+          //   required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <div className={styles.field}>
+        <button disabled={loggingIn}>
+          {' '}
+          {loggingIn ? 'Logging in...' : 'Log In'}
+        </button>
+      </div>
     </form>
+  );
 };
 
 export default Login;
