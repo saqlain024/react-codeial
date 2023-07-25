d0c9f157d1d2d97ea1305e69b1d0000cdbf293c9
Saklain Ahmad <saklainhmd@gmail.com>
2023-07-25 18:20:26 +0530

    Auth Context

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import { ToastProvider } from 'react-toast-notifications';
 
 import './styles/index.css';
 import { App } from './components';
-
+import { AuthProvider } from './providers/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ToastProvider autoDismiss autoDismissTimeout={5000} placement='top-left'>
-      <App />
+    <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ToastProvider>
   </React.StrictMode>
 );
